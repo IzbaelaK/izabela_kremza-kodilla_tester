c3ea7c5d5c3fc0de5484c870ff370dc2cb9a71b1
Izabela Kremza <izabela.kremza@interia.pl>
2022-10-19 12:13:06 +0200

    Initial version

---
+++ kodilla-intro/kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,33 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastOne(int value) {
+        return (this.grades[this.size]);
+
+    }
+    public double averageOfgrades() {
+        int suma = 0;
+        for (int i = 0; i < this.size; i++); {
+            suma+= grades [i];
+                }
+                int average = suma/(size+1);
+        return (averageOfgrades());
+
+    }
+
+
+    }

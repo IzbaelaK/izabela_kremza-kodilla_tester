64d839331b9783c1454684aec7a2a42d2ef62090
Izabela Kremza <izabela.kremza@interia.pl>
2022-11-16 22:22:45 +0100

    initial version

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int square(int a, int square) {
+        return (a * a) ;
+
+    }
+    public int square() {
+        return 0;
+    }
+}

4c08b4c9050937153ee14787492743dc1bd2c13a
Izabela Kremza <izabela.kremza@interia.pl>
2023-01-05 10:55:33 +0100

    initial version

--- kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTest.java
@@ -19,8 +19,12 @@ public class ApplicationTest {
             Optional<Teacher> optionalTeacher = Optional.ofNullable(student.getTeacher());
             String name = optionalTeacher.orElse(new Teacher("<undefined>")).getName();
             //then
+            students.add(new Student("Jan Kowslski", new Teacher ("Elwira Moskal")));
             students.add(new Student("Katarzyna Piorun", null));
             students.add(new Student("Jolanta Szpak", null));
+            students.add(new Student("Maria Kucharska", new Teacher ("Bogusław Rząsa")));
+            students.add(new Student("Grzegorz Nowak", new Teacher ("Wojciech Adamczyk")));
+
             System.out.println("Uczeń: " + student.getName() + ", " + "nauczyciel: " + name);
         }
     }

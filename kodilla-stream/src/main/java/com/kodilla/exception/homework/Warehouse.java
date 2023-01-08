04d1fc748bfbb70829171bd597d8b337c78b2e07
Izabela Kremza <izabela.kremza@interia.pl>
2023-01-08 18:21:36 +0100

    initial version

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -1,35 +1,23 @@
 package com.kodilla.exception.homework;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Warehouse {
-    Set<Order> orders = new HashSet<>();
+    List<Order> orders = new ArrayList<>();
 
-    public Set<Order> addOrder(Order order) {
+    public void addOrder(Order order) {
         orders.add(order);
-        return orders;
     }
-    public Set<Order> getOrder(String number) {
-        try{ getOrders()
-                .stream()
-                .filter(n -> n.getNumber().equals(number))
-                .findAny().orElseThrow();
-
-        } catch (Exception e) {
-            System.out.println("Zamówienie nie istnieje. Spróbuj innego numeru.");
-        }
-        return getOrders();
+    public Order getOrder (String number) throws OrderDoesntExistException {
+        return orders.stream().filter(o->o.getNumber().equals(number)).findAny().orElseThrow(()->new OrderDoesntExistException());
     }
 
     public String isOrderInUse (String number) throws OrderDoesntExistException {
         if (getOrder(number).equals(number))
             return getOrder(number).toString();
         throw new OrderDoesntExistException();
-    }
 
-    public Set<Order> getOrders() {
-        return orders;
     }
 
     @Override
--- kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -3,20 +3,12 @@ package com.kodilla.exception.homework;
 public class WarehouseApp {
     public static void main(String[] args) throws OrderDoesntExistException {
         Warehouse warehouse = new Warehouse();
-        warehouse.addOrder(new Order("Order1"));
-        warehouse.addOrder(new Order("Order2"));
-        warehouse.addOrder(new Order("Order3"));
-        warehouse.addOrder(new Order("Order4"));
-        warehouse.addOrder(new Order("Order5"));
-        warehouse.addOrder(new Order("Order6"));
-
-        warehouse.getOrder( "Order5");
         try {
-            warehouse.isOrderInUse("Order10");
+            Order findOrder = warehouse.getOrder("567DD");
+            System.out.println("Order found!");
         } catch (OrderDoesntExistException e) {
-            System.out.println("Zamówienie nie istnieje. Spróbuj innego numeru.");
+            System.out.println("Order doesnt exist!");
+        } finally {
+            System.out.println("Hope result is fine!");
         }
-        System.out.println("Twoje zamówienie. Sprawdź inny.");
-    }
-}
-
+    }}
